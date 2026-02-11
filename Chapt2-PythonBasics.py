1089b6ec615ab33228bde39cdd0177f946467679
xitsjacob-school <jpears@student.sfccmo.edu>
2026-02-11 16:42:11 +0000

    finished assignment

--- Chapt2-PythonBasics.py
+++ Chapt2-PythonBasics.py
@@ -1,8 +1,8 @@
 # --------------------------------------------
-# Name:
-# Date:
+# Name: Jacob Pears
+# Date: 02/11/2026
 # Program: Chapter 2 Practice
-# Description:
+# Description: Pratice questions
 # Complete each section by following the
 # directions in the comments.
 # --------------------------------------------
@@ -15,9 +15,10 @@
 # 2. Store your name as a string in the variable
 # 3. Use print() to display: Hello, <name>
 
+name = input("What is your name? ")
 
-print()  # blank line for readability
-
+print(f"Hello, {name}")  # blank line for readability
+print()
 
 # ------------------------------------------------
 # Practice 2: Input and output
@@ -26,10 +27,10 @@ print()  # blank line for readability
 # 1. Ask the user to enter their favorite color
 # 2. Store the input in a variable
 # 3. Print a sentence that includes the color
+fav_color = input("What is your favorite color? ")
 
-
+print(f"Color is {fav_color}")
 print()
-
 
 # ------------------------------------------------
 # Practice 3: Adding two numbers
@@ -42,9 +43,12 @@ print()
 # 5. Add the two numbers together
 # 6. Print the total
 
+first_num = int(input("Pick a number: "))
+second_num = int(input("Pick a second number: "))
 
+num_total = first_num + second_num
+print(num_total)
 print()
-
 
 # ------------------------------------------------
 # Practice 4: Calculations with variables
@@ -57,9 +61,13 @@ print()
 # 5. Calculate the final price
 # 6. Print the final price
 
+price = float(input("Price: "))
+tax_rate = 0.08
 
+total = price + (price * tax_rate)
+
+print(total)
 print()
-
 
 # ------------------------------------------------
 # Practice 5: Formatted output with f-strings
@@ -71,3 +79,10 @@ print()
 # 4. Calculate weekly pay
 # 5. Use an f-string to display the result
 #    (Round to 2 decimal places)
+
+hours_worked = float(input("Hours worked: "))
+hourly_pay = float(input("Hourly pay: "))
+weekly_pay = hours_worked * hourly_pay
+
+print(f"{hours_worked} worked. {hourly_pay} hourly pay. Total Week Pay: {weekly_pay}")
+print()
